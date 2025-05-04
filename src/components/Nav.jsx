d02b3390f279f549971873f488c919ba6008bc11
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-05-04 22:28:25 +0200

    change to local API

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,21 @@
 import Link from 'next/link'
-
-const url = process.env.REST_API
-const endpoint = '/api/globals/header?depth=2&draft=false&locale=undefined'
+import { getPayload } from 'payload'
+import config from '@payload-config'
 
 export async function Nav() {
-  const navItems = await fetch(url + endpoint)
-    .then((res) => res.json())
+  const payload = await getPayload({ config })
+  const nav = await payload
+    .findGlobal({
+      slug: 'header', // required
+      depth: 2,
+      showHiddenFields: true,
+    })
     .then((data) => data.items)
 
   return (
     <header className="full-bleed bg-slate-400 p-1">
       <nav className="flex gap-6">
-        {navItems.map((item, id) => {
+        {nav.map((item, id) => {
           return (
             <Link key={id} href={item.page.path}>
               {item.page.title}
