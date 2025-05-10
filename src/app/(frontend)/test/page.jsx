0e5daca084115c5146b039d168f4ccf1752f7c8d
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-05-10 02:25:00 +0200

    .

--- src/app/(frontend)/test/page.jsx
+++ src/app/(frontend)/test/page.jsx
@@ -2,10 +2,5 @@ import { getPayload } from 'payload'
 import config from '@payload-config'
 
 export default async function Home() {
-  return (
-    <main className="full-bleed">
-      <Hero />
-      {/* <MiniBio /> */}
-    </main>
-  )
+  return <main className="full-bleed">{/* <MiniBio /> */}</main>
 }
